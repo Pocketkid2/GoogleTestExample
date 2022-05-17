b8897a1ca9aa3ff0b6e83fbf55487e770e938aaa
Adam Taylor <afranklint16@gmail.com>
2022-05-17 14:36:16 -0600

    Revert "Break code to check that tests fail"
    This reverts commit 14b05bb59be8e5b0b4dc5e24204b1622af1e7925.

--- exponent.cpp
+++ exponent.cpp
@@ -7,10 +7,10 @@ uint64_t exp_by_squaring(uint64_t base, uint64_t exp) {
     if (exp == 0) {
         return 1;
     } else if (exp % 2) {
-        // If exponent is even
-        return exp_by_squaring(base * base, exp / 2);
-    } else {
         // If exponent is odd
         return base * exp_by_squaring(base * base, (exp - 1) / 2);
+    } else {
+        // If exponent is even
+        return exp_by_squaring(base * base, exp / 2);
     }
 }
